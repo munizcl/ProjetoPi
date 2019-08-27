9f075a767ea4181bd34d775245c277a00789ccad
Algalord666 <w1lliam.m4rques@gmail.com>
2019-08-27 14:59:53 -0300

    site mudacças basicas

--- Site/app/routes/dashboard.js
+++ Site/app/routes/dashboard.js
@@ -1,5 +1,33 @@
 module.exports = (application) =>{
-    application.get('/dashboard', (req, res)=>{
-        res.render('../view/dashboard/index.ejs');
+    application.get('/dashboard', (req, res, err)=>{
+        res.render('../view/dashboard/index.ejs');      
+    });
+
+    application.get('/login', (req, res)=>{
+        res.render('../view/dashboard/login.ejs');
+    });
+
+    application.get('/register', (req, res)=>{
+        res.render('../view/dashboard/register.ejs');
+    });
+
+    application.get('/err', (req, res)=>{
+        res.render('../view/dashboard/404.ejs');
+    });
+
+    application.get('/table', (req, res)=>{
+        res.render('../view/dashboard/tables.ejs');
+    });
+
+    application.get('/blank', (req, res)=>{
+        res.render('../view/dashboard/blank.ejs');
+    });
+
+    application.get('/charts', (req, res)=>{
+        res.render('../view/dashboard/charts.ejs');
+    });
+
+    application.get('/fgps', (req, res)=>{
+        res.render('../view/dashboard/forgot-password.ejs');
     });
 }
